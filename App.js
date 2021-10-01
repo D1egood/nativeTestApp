53cb8c50b9a15441834ef252336033f878c352e3
AbsoluteC2H6O <alfredofp63@gmail.com>
2021-10-01 09:47:53 -0400

    Hello, this is the init of the repositorie

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert, ImagePickerIOS } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import * as ImagePicker from 'expo-image-picker'
+import * as sharing from 'expo-sharing'
 
-export default function App() {
+
+const App = () => {
+  
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  
+      if(permissionResult.granted === false){
+        alert('Permission to access camera is required');
+        return;
+      }
+      const pickerResult = await ImagePicker.launchImageLibraryAsync();
+      if(pickerResult.cancelled === true){
+        return;
+      }
+
+      setSelectedImage({LocalUri: pickerResult.uri});
+    };
+
+    const openShareDialog = async () => {
+      if( !(sharing.isAvailableAsync()) ){
+        alert("sharing is not available");
+        return;
+      }
+
+      await sharing.shareAsync(selectedImage.LocalUri);
+    };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={styles.head}>
+      <Text style={styles.text}>Pick an image Perro!</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}><Image source={{ uri: selectedImage !== null ? selectedImage.LocalUri
+      :'https://i.picsum.photos/id/471/200/300.jpg?grayscale&hmac=VVP5WUlIS24-8K_U2RsF6As9K61ST17q4mZZSTTzkVk' }} style={styles.img} /></TouchableOpacity>
+      
+      <TouchableOpacity>
+        { selectedImage ? (<TouchableOpacity style={styles.bu} onPress={openShareDialog}><Text>Share me</Text></TouchableOpacity>) : (<View />)}
+        </TouchableOpacity>
+    </View>);
+
+};
 
 const styles = StyleSheet.create({
-  container: {
+  head: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkgray',
+  },
+  text: {
+    fontSize: 30,
+    fontFamily: "serif",
+    color: 'black',
+  },
+  img: {
+    margin:20,
+    width: 200,
+    height: 200,
+    //resizeMode: 'contain',
+    borderWidth:3,
+    borderColor: 'black',
+    borderRadius: 100,
+  },
+  bu: {
+    marginTop:10,
+    padding: 7,
+    fontSize:24,
+    borderRadius: 10,
+    borderWidth: 5,
+    borderColor:'black',
   },
 });
+
+export default App;
---
+++ READme.md
@@ -0,0 +1,4 @@
+This is my first mobile app Enjoy!
+this code is made on native and using expo CLI
+
+for now it just share pictures in the phone with another programs on the cellphone!.
